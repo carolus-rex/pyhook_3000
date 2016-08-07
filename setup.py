58abd12b0473b62174851976fcd05cb92b01bad5
remoto <er2950@hotmail.com>
2016-08-07 04:42:00 -0500

    Modify LICENSE.txt and setup.py
    Adding me to the license and replace the original author's and
    mantainer's metadata with mine.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-'''pyHook: Python wrapper for out-of-context input hooks in Windows
+"""pyHook: Python wrapper for out-of-context input hooks in Windows
 
 The pyHook package provides callbacks for global mouse and keyboard events in Windows. Python
 applications register event handlers for user input events such as left mouse down, left mouse up,
 key down, etc. and set the keyboard and/or mouse hook. The underlying C library reports information
 like the time of the event, the name of the window in which the event occurred, the value of the
 event, any keyboard modifiers, etc.
-'''
+"""
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
@@ -22,22 +22,20 @@ from setuptools import setup, Extension
 libs = ['user32']
 doclines = __doc__.split('\n')
 
-setup(name='pyHook',
-      version='1.5.1',
-      maintainer='Daniel Folkinshteyn',
-      maintainer_email='nanotube@users.sf.net',
-      author='Peter Parente',
-      author_email='parente@cs.unc.edu',
-      url='http://pyhook.sourceforge.net',
-      download_url='http://www.sourceforge.net/projects/pyhook',
+setup(name='pyHook_3000',
+      version='2',
+      maintainer='carolus-rex',
+      author='Peter Parente (original author), Cosmo Du(first fork author), carolus-rex(second fork (this) author)',
+      author_email='carolus-rex-code@protonmail.com',
+      url='https://github.com/carolus-rex/pyhook_3000',
       license='http://www.opensource.org/licenses/mit-license.php',
       platforms=['Win32'],
-      description = doclines[0],
-      classifiers = filter(None, classifiers.split('\n')),
-      long_description = ' '.join(doclines[2:]),
-      packages = ['pyHook'],
-      package_dir = {'pyHook' : ""},
-      ext_modules = [Extension('pyHook._cpyHook', ['cpyHook.i'], libraries=libs)],
-      data_files=[('Lib/site-packages/pyHook', ['LICENSE.txt', 'README.txt','CHANGELOG.txt'])],
+      description=doclines[0],
+      classifiers=filter(None, classifiers.split('\n')),
+      long_description=' '.join(doclines[2:]),
+      packages=['pyHook'],
+      package_dir={'pyHook': ""},
+      ext_modules=[Extension('pyHook._cpyHook', ['cpyHook.i'], libraries=libs)],
+      data_files=[('Lib/site-packages/pyHook', ['LICENSE.txt', 'README.txt', 'CHANGELOG.txt', 'README.md'])],
       use_2to3=True
-      )
+)
